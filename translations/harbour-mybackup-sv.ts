b55e854f9636a91540482b88906eb7b56e605687
Åke Engelbrektson <eson57@users.noreply.github.com>
2021-01-10 08:58:25 +0100

    Create harbour-mybackup-sv.ts
    Add Swedish translation, please.
    Thanks for this long awaited app! Should have been system standard.

---
+++ translations/harbour-mybackup-sv.ts
@@ -0,0 +1,127 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name></name>
+    <message id="mybackup-app_name">
+        <source>My Backup</source>
+        <extracomment>Application title</extracomment>
+        <translation>My Backup</translation>
+    </message>
+    <message id="mybackup-main_page-title">
+        <source>Backup items</source>
+        <extracomment>Main page title</extracomment>
+        <translation>Säkerhetskopieringsobjekt</translation>
+    </message>
+    <message id="mybackup-main_page-add_item">
+        <source>Add new item</source>
+        <extracomment>Pressable footer label</extracomment>
+        <translation>Lägg till nytt objekt</translation>
+    </message>
+    <message id="mybackup-entry_description-application">
+        <source>Application</source>
+        <extracomment>Backup item description</extracomment>
+        <translation>App</translation>
+    </message>
+    <message id="mybackup-entry_description-home">
+        <source>Home directory</source>
+        <extracomment>Backup item description</extracomment>
+        <translation>Hemkatalog</translation>
+    </message>
+    <message id="mybackup-entry_description-directory">
+        <source>Directory</source>
+        <extracomment>Backup item description</extracomment>
+        <translation>Mapp</translation>
+    </message>
+    <message id="mybackup-entry_description-file">
+        <source>File</source>
+        <extracomment>Backup item description</extracomment>
+        <translation>Fil</translation>
+    </message>
+    <message id="mybackup-entry_description-configuration_group">
+        <source>Configuration group</source>
+        <extracomment>Backup item description</extracomment>
+        <translation>Konfigurationsgrupp</translation>
+    </message>
+    <message id="mybackup-entry_description-configuration_value">
+        <source>Configuration value</source>
+        <extracomment>Backup item description</extracomment>
+        <translation>Konfigurationsvärde</translation>
+    </message>
+    <message id="mybackup-menu-remove">
+        <source>Remove</source>
+        <extracomment>Context menu item</extracomment>
+        <translation>Ta bort</translation>
+    </message>
+    <message id="mybackup-remorse-removing">
+        <source>Removing</source>
+        <extracomment>Remorse item text</extracomment>
+        <translation>Tar bort</translation>
+    </message>
+    <message id="mybackup-add_page-title">
+        <source>Add backup item</source>
+        <extracomment>Add backup item page title</extracomment>
+        <translation>Lägg till objektets sidtitel</translation>
+    </message>
+    <message id="mybackup-add_page-item_app">
+        <source>Application</source>
+        <extracomment>Add backup item page label (backup item type)</extracomment>
+        <translation>App</translation>
+    </message>
+    <message id="mybackup-add_page-item_path">
+        <source>File or directory</source>
+        <extracomment>Add backup item page label (backup item type)</extracomment>
+        <translation>Fil eller mapp</translation>
+    </message>
+    <message id="mybackup-add_page-item_config">
+        <source>Configuration</source>
+        <extracomment>Add backup item page label (backup item type)</extracomment>
+        <translation>Konfiguration</translation>
+    </message>
+    <message id="mybackup-add_app-placeholder_text">
+        <source>None of the installed applications are integrated with My Backup</source>
+        <extracomment>Placeholder text</extracomment>
+        <translation>Ingen av de installerade apparna är integrerade i My Backup</translation>
+    </message>
+    <message id="mybackup-add_app-placeholder_hint">
+        <source>Try installing Counter, Foil Notes, Foil Pics or Foil Auth application from either Jolla Store or OpenRepos.net</source>
+        <extracomment>Placeholder hint</extracomment>
+        <translation>Försök med att installera Counter, Foil Notes, Foil Pics eller Foil Auth från antingen Jolla Store eller OpenRepos.net</translation>
+    </message>
+    <message id="mybackup-item_info-section-files">
+        <source>Files</source>
+        <extracomment>Backup item info section</extracomment>
+        <translation>Filer</translation>
+    </message>
+    <message id="mybackup-item_info-section-config">
+        <source>Configuration</source>
+        <extracomment>Backup item info section</extracomment>
+        <translation>Konfiguration</translation>
+    </message>
+    <message id="mybackup-configure-button">
+        <source>Install backup unit</source>
+        <extracomment>Button label</extracomment>
+        <translation>Installera säkerhetskopieringsenhet</translation>
+    </message>
+    <message id="mybackup-cover-not_configured">
+        <source>Not configured</source>
+        <extracomment>Cover page label</extracomment>
+        <translation>Inte konfigurerad</translation>
+    </message>
+    <message id="mybackup-cover-last_backup">
+        <source>Last backup</source>
+        <extracomment>Cover page label</extracomment>
+        <translation>Senaste säkerhetskopia</translation>
+    </message>
+    <message id="mybackup-cover-last_restore">
+        <source>Last restore</source>
+        <extracomment>Cover page label</extracomment>
+        <translation>Senaste återställning</translation>
+    </message>
+    <message id="mybackup-cover-nothing_backed_up">
+        <source>Nothing has been backed up or restored yet</source>
+        <extracomment>Cover page label</extracomment>
+        <translation>Inget har säkerhetskopierats eller återställts ännu</translation>
+    </message>
+</context>
+</TS>
