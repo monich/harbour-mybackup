470f150a50227adb0f73faa30a00bb7d636c87c8
A <hucul@o2.pl>
2021-01-10 10:31:23 +0100

    Create harbour-mybackup-pl.ts

---
+++ translations/harbour-mybackup-pl.ts
@@ -0,0 +1,127 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl">
+<context>
+    <name></name>
+    <message id="mybackup-app_name">
+        <source>My Backup</source>
+        <extracomment>Application title</extracomment>
+        <translation>My Backup</translation>
+    </message>
+    <message id="mybackup-main_page-title">
+        <source>Backup items</source>
+        <extracomment>Main page title</extracomment>
+        <translation>Pozycje kopii zapasowej</translation>
+    </message>
+    <message id="mybackup-main_page-add_item">
+        <source>Add new item</source>
+        <extracomment>Pressable footer label</extracomment>
+        <translation>Dodaj nową pozycję</translation>
+    </message>
+    <message id="mybackup-entry_description-application">
+        <source>Application</source>
+        <extracomment>Backup item description</extracomment>
+        <translation>Aplikacja</translation>
+    </message>
+    <message id="mybackup-entry_description-home">
+        <source>Home directory</source>
+        <extracomment>Backup item description</extracomment>
+        <translation>Katalog domowy</translation>
+    </message>
+    <message id="mybackup-entry_description-directory">
+        <source>Directory</source>
+        <extracomment>Backup item description</extracomment>
+        <translation>Katalog</translation>
+    </message>
+    <message id="mybackup-entry_description-file">
+        <source>File</source>
+        <extracomment>Backup item description</extracomment>
+        <translation>Plik</translation>
+    </message>
+    <message id="mybackup-entry_description-configuration_group">
+        <source>Configuration group</source>
+        <extracomment>Backup item description</extracomment>
+        <translation>Grupa konfiguracji</translation>
+    </message>
+    <message id="mybackup-entry_description-configuration_value">
+        <source>Configuration value</source>
+        <extracomment>Backup item description</extracomment>
+        <translation>Wartość konfiguracji</translation>
+    </message>
+    <message id="mybackup-menu-remove">
+        <source>Remove</source>
+        <extracomment>Context menu item</extracomment>
+        <translation>Usuń</translation>
+    </message>
+    <message id="mybackup-remorse-removing">
+        <source>Removing</source>
+        <extracomment>Remorse item text</extracomment>
+        <translation>Usuwanie</translation>
+    </message>
+    <message id="mybackup-add_page-title">
+        <source>Add backup item</source>
+        <extracomment>Add backup item page title</extracomment>
+        <translation>Dodaj pozycję kopii zapasowej</translation>
+    </message>
+    <message id="mybackup-add_page-item_app">
+        <source>Application</source>
+        <extracomment>Add backup item page label (backup item type)</extracomment>
+        <translation>Aplikacja</translation>
+    </message>
+    <message id="mybackup-add_page-item_path">
+        <source>File or directory</source>
+        <extracomment>Add backup item page label (backup item type)</extracomment>
+        <translation>Plik lub katalog</translation>
+    </message>
+    <message id="mybackup-add_page-item_config">
+        <source>Configuration</source>
+        <extracomment>Add backup item page label (backup item type)</extracomment>
+        <translation>Konfiguracja</translation>
+    </message>
+    <message id="mybackup-add_app-placeholder_text">
+        <source>None of the installed applications are integrated with My Backup</source>
+        <extracomment>Placeholder text</extracomment>
+        <translation>Żadna zainstalowana aplikacja nie jest zintegrowana z My Backup</translation>
+    </message>
+    <message id="mybackup-add_app-placeholder_hint">
+        <source>Try installing Counter, Foil Notes, Foil Pics or Foil Auth application from either Jolla Store or OpenRepos.net</source>
+        <extracomment>Placeholder hint</extracomment>
+        <translation>Spróbuj zainstalować Counter, Foil Notes, Foil Pics lub Foil Auth ze sklepu Jolla lub OpenRepos.net</translation>
+    </message>
+    <message id="mybackup-item_info-section-files">
+        <source>Files</source>
+        <extracomment>Backup item info section</extracomment>
+        <translation>Pliki</translation>
+    </message>
+    <message id="mybackup-item_info-section-config">
+        <source>Configuration</source>
+        <extracomment>Backup item info section</extracomment>
+        <translation>Konfiguracja</translation>
+    </message>
+    <message id="mybackup-configure-button">
+        <source>Install backup unit</source>
+        <extracomment>Button label</extracomment>
+        <translation>Zainstaluj jednostkę zapasową</translation>
+    </message>
+    <message id="mybackup-cover-not_configured">
+        <source>Not configured</source>
+        <extracomment>Cover page label</extracomment>
+        <translation>Nie skonfigurowane</translation>
+    </message>
+    <message id="mybackup-cover-last_backup">
+        <source>Last backup</source>
+        <extracomment>Cover page label</extracomment>
+        <translation>Ostatnia kopia zapasowa</translation>
+    </message>
+    <message id="mybackup-cover-last_restore">
+        <source>Last restore</source>
+        <extracomment>Cover page label</extracomment>
+        <translation>Ostatnie przywracanie kopii</translation>
+    </message>
+    <message id="mybackup-cover-nothing_backed_up">
+        <source>Nothing has been backed up or restored yet</source>
+        <extracomment>Cover page label</extracomment>
+        <translation>Do tej pory nie wykonanao żadnej kopii zapasowej, ani nie przywrócono kopii</translation>
+    </message>
+</context>
+</TS>
