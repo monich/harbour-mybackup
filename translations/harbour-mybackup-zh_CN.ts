630c2a22b6b26b6a36290a7ab37d1a04292a8c45
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2021-01-11 04:14:41 +0800

    Create harbour-mybackup-zh_CN.ts

---
+++ translations/harbour-mybackup-zh_CN.ts
@@ -0,0 +1,132 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name></name>
+    <message id="mybackup-app_name">
+        <source>My Backup</source>
+        <extracomment>Application title</extracomment>
+        <translation>My Backup（我的备份）</translation>
+    </message>
+    <message id="mybackup-main_page-title">
+        <source>Backup items</source>
+        <extracomment>Main page title</extracomment>
+        <translation>备份项目</translation>
+    </message>
+    <message id="mybackup-main_page-description">
+        <source>To be added to the backup</source>
+        <extracomment>Main page description</extracomment>
+        <translation>等待添加到备份</translation>
+    </message>
+    <message id="mybackup-main_page-add_item">
+        <source>Add new item</source>
+        <extracomment>Pressable footer label</extracomment>
+        <translation>添加新项目</translation>
+    </message>
+    <message id="mybackup-entry_description-application">
+        <source>Application</source>
+        <extracomment>Backup item description</extracomment>
+        <translation>软件</translation>
+    </message>
+    <message id="mybackup-entry_description-home">
+        <source>Home directory</source>
+        <extracomment>Backup item description</extracomment>
+        <translation>Home 目录</translation>
+    </message>
+    <message id="mybackup-entry_description-directory">
+        <source>Directory</source>
+        <extracomment>Backup item description</extracomment>
+        <translation>目录</translation>
+    </message>
+    <message id="mybackup-entry_description-file">
+        <source>File</source>
+        <extracomment>Backup item description</extracomment>
+        <translation>文件</translation>
+    </message>
+    <message id="mybackup-entry_description-configuration_group">
+        <source>Configuration group</source>
+        <extracomment>Backup item description</extracomment>
+        <translation>配置群组</translation>
+    </message>
+    <message id="mybackup-entry_description-configuration_value">
+        <source>Configuration value</source>
+        <extracomment>Backup item description</extracomment>
+        <translation>配置值</translation>
+    </message>
+    <message id="mybackup-menu-remove">
+        <source>Remove</source>
+        <extracomment>Context menu item</extracomment>
+        <translation>移除</translation>
+    </message>
+    <message id="mybackup-remorse-removing">
+        <source>Removing</source>
+        <extracomment>Remorse item text</extracomment>
+        <translation>正在移除</translation>
+    </message>
+    <message id="mybackup-add_page-title">
+        <source>Add backup item</source>
+        <extracomment>Add backup item page title</extracomment>
+        <translation>Add backup item</translation>
+    </message>
+    <message id="mybackup-add_page-item_app">
+        <source>Application</source>
+        <extracomment>Add backup item page label (backup item type)</extracomment>
+        <translation>软件</translation>
+    </message>
+    <message id="mybackup-add_page-item_path">
+        <source>File or directory</source>
+        <extracomment>Add backup item page label (backup item type)</extracomment>
+        <translation>文件或目录</translation>
+    </message>
+    <message id="mybackup-add_page-item_config">
+        <source>Configuration</source>
+        <extracomment>Add backup item page label (backup item type)</extracomment>
+        <translation>配置</translation>
+    </message>
+    <message id="mybackup-add_app-placeholder_text">
+        <source>None of the installed applications are integrated with My Backup</source>
+        <extracomment>Placeholder text</extracomment>
+        <translation>已安装的软件均未与 My Backup（我的备份） 整合</translation>
+    </message>
+    <message id="mybackup-add_app-placeholder_hint">
+        <source>Try installing Counter, Foil Notes, Foil Pics or Foil Auth application from either Jolla Store or OpenRepos.net</source>
+        <extracomment>Placeholder hint</extracomment>
+        <translation>尝试从 Jolla 商店或 OpenRepos.net 安装 Counter 、Foil Notes 、Foil Pics 或 Foil Auth 。</translation>
+    </message>
+    <message id="mybackup-item_info-section-files">
+        <source>Files</source>
+        <extracomment>Backup item info section</extracomment>
+        <translation>文件</translation>
+    </message>
+    <message id="mybackup-item_info-section-config">
+        <source>Configuration</source>
+        <extracomment>Backup item info section</extracomment>
+        <translation>配置</translation>
+    </message>
+    <message id="mybackup-configure-button">
+        <source>Install backup unit</source>
+        <extracomment>Button label</extracomment>
+        <translation>安装备份组件</translation>
+    </message>
+    <message id="mybackup-cover-not_configured">
+        <source>Not configured</source>
+        <extracomment>Cover page label</extracomment>
+        <translation>尚未配置</translation>
+    </message>
+    <message id="mybackup-cover-last_backup">
+        <source>Last backup</source>
+        <extracomment>Cover page label</extracomment>
+        <translation>最近备份</translation>
+    </message>
+    <message id="mybackup-cover-last_restore">
+        <source>Last restore</source>
+        <extracomment>Cover page label</extracomment>
+        <translation>最近恢复</translation>
+    </message>
+    <message id="mybackup-cover-nothing_backed_up">
+        <source>Nothing has been backed up or restored yet</source>
+        <extracomment>Cover page label</extracomment>
+        <translation>尚未备份或恢复任何东西</translation>
+    </message>
+</context>
+</TS>
