a4a6918700625169323a8c3f6b03a70551c79192
Slava Monich <slava@monich.com>
2021-01-11 00:03:53 +0200

    [translation] Marked Swedish translation as finished

--- translations/harbour-mybackup-sv.ts
+++ translations/harbour-mybackup-sv.ts
@@ -16,7 +16,7 @@
     <message id="mybackup-main_page-description">
         <source>To be added to the backup</source>
         <extracomment>Main page description</extracomment>
-        <translation type="unfinished">Att läggas till i säkerhetskopian</translation>
+        <translation>Att läggas till i säkerhetskopian</translation>
     </message>
     <message id="mybackup-main_page-add_item">
         <source>Add new item</source>
