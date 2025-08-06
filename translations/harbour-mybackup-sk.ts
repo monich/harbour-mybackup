7342bfb5c19580e7ab96687094cdc013e8b5e1b0
okruhliak <ladislav@hodas.sk>
2025-08-06 07:36:04 +0200

    Update harbour-mybackup-sk.ts

---
+++ translations/harbour-mybackup-sk.ts
@@ -0,0 +1,132 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sk">
+<context>
+    <name></name>
+    <message id="mybackup-app_name">
+        <source>My Backup</source>
+        <extracomment>Application title</extracomment>
+        <translation>My Backup</translation>
+    </message>
+    <message id="mybackup-main_page-title">
+        <source>Backup items</source>
+        <extracomment>Main page title</extracomment>
+        <translation>Položky zálohovania</translation>
+    </message>
+    <message id="mybackup-main_page-description">
+        <source>To be added to the backup</source>
+        <extracomment>Main page description</extracomment>
+        <translation>Pridať na zálohovanie</translation>
+    </message>
+    <message id="mybackup-main_page-add_item">
+        <source>Add new item</source>
+        <extracomment>Pressable footer label</extracomment>
+        <translation>Pridať novú položku</translation>
+    </message>
+    <message id="mybackup-entry_description-application">
+        <source>Application</source>
+        <extracomment>Backup item description</extracomment>
+        <translation>Aplikácia</translation>
+    </message>
+    <message id="mybackup-entry_description-home">
+        <source>Home directory</source>
+        <extracomment>Backup item description</extracomment>
+        <translation>Domovský adresár</translation>
+    </message>
+    <message id="mybackup-entry_description-directory">
+        <source>Directory</source>
+        <extracomment>Backup item description</extracomment>
+        <translation>Adresár</translation>
+    </message>
+    <message id="mybackup-entry_description-file">
+        <source>File</source>
+        <extracomment>Backup item description</extracomment>
+        <translation>Súbor</translation>
+    </message>
+    <message id="mybackup-entry_description-configuration_group">
+        <source>Configuration group</source>
+        <extracomment>Backup item description</extracomment>
+        <translation>Skupina konfigurácie</translation>
+    </message>
+    <message id="mybackup-entry_description-configuration_value">
+        <source>Configuration value</source>
+        <extracomment>Backup item description</extracomment>
+        <translation>Hodnota konfigurácie</translation>
+    </message>
+    <message id="mybackup-menu-remove">
+        <source>Remove</source>
+        <extracomment>Context menu item</extracomment>
+        <translation>Odstrániť</translation>
+    </message>
+    <message id="mybackup-remorse-removing">
+        <source>Removing</source>
+        <extracomment>Remorse item text</extracomment>
+        <translation>Odstraňovanie</translation>
+    </message>
+    <message id="mybackup-add_page-title">
+        <source>Add backup item</source>
+        <extracomment>Add backup item page title</extracomment>
+        <translation>Pridať položku zálohovania</translation>
+    </message>
+    <message id="mybackup-add_page-item_app">
+        <source>Application</source>
+        <extracomment>Add backup item page label (backup item type)</extracomment>
+        <translation>Aplikácia</translation>
+    </message>
+    <message id="mybackup-add_page-item_path">
+        <source>File or directory</source>
+        <extracomment>Add backup item page label (backup item type)</extracomment>
+        <translation>Súbor alebo adresár</translation>
+    </message>
+    <message id="mybackup-add_page-item_config">
+        <source>Configuration</source>
+        <extracomment>Add backup item page label (backup item type)</extracomment>
+        <translation>Konfigurácia</translation>
+    </message>
+    <message id="mybackup-add_app-placeholder_text">
+        <source>None of the installed applications are integrated with My Backup</source>
+        <extracomment>Placeholder text</extracomment>
+        <translation>Žiadna z inštalovaných aplikácií nie je zaradená do My Backup</translation>
+    </message>
+    <message id="mybackup-add_app-placeholder_hint">
+        <source>Try installing Counter, Foil Notes, Foil Pics or Foil Auth application from either Jolla Store or OpenRepos.net</source>
+        <extracomment>Placeholder hint</extracomment>
+        <translation>Skúste inštalovať Counter, Foil Notes, Foil Pics lub Foil Auth zo skladu Jolla alebo OpenRepos.net</translation>
+    </message>
+    <message id="mybackup-item_info-section-files">
+        <source>Files</source>
+        <extracomment>Backup item info section</extracomment>
+        <translation>Súbory</translation>
+    </message>
+    <message id="mybackup-item_info-section-config">
+        <source>Configuration</source>
+        <extracomment>Backup item info section</extracomment>
+        <translation>Konfigurácia</translation>
+    </message>
+    <message id="mybackup-configure-button">
+        <source>Install backup unit</source>
+        <extracomment>Button label</extracomment>
+        <translation>Nainštalujte záložnú jednotku</translation>
+    </message>
+    <message id="mybackup-cover-not_configured">
+        <source>Not configured</source>
+        <extracomment>Cover page label</extracomment>
+        <translation>Nie je nakonfigurované</translation>
+    </message>
+    <message id="mybackup-cover-last_backup">
+        <source>Last backup</source>
+        <extracomment>Cover page label</extracomment>
+        <translation>Ostatná záloha</translation>
+    </message>
+    <message id="mybackup-cover-last_restore">
+        <source>Last restore</source>
+        <extracomment>Cover page label</extracomment>
+        <translation>Ostatná obnova</translation>
+    </message>
+    <message id="mybackup-cover-nothing_backed_up">
+        <source>Nothing has been backed up or restored yet</source>
+        <extracomment>Cover page label</extracomment>
+        <translation>Nič nebolo zálohované ani obnovené</translation>
+    </message>
+</context>
+</TS>
